cc8b94edb43b9120de0e8abfe968e9ac94c0a887
Mrunal <mrunalrawade05@gmail.com>
2026-01-28 14:00:53 +0530

    middleware(auth): add verifyJWT to validate access token and attach user to req

---
+++ Backend_with_MongoDB/VideoTube/src/middlewares/auth.middleware.js
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.models.js";
+import { ApiError } from "../utils/api-error.js";
+import { ApiResponse } from "../utils/api-response.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+export const verifyJWT = asyncHandler(async (req, _, next) => {
+  const token =
+    req.cookies.accessToken ||
+    req.header("Authorization")?.replace("Bearer ", "");
+
+  if (!token) {
+    throw new ApiError(401, "Unauthorized request");
+  }
+
+  try {
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!user) {
+      throw new ApiError(404, "User not found");
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid access token");
+  }
+});
